24c423fbc815579b64c37dedd1061dade3b84ad1
jeonginy <86006588+jeonginy@users.noreply.github.com>
2021-07-02 10:18:41 +0900

    Update BLankTest.java
    This project is to remove the blank with two different ways

--- BLankTest.java
+++ BLankTest.java
@@ -3,24 +3,24 @@ public class BLankTest {
 	public static void main(String[] args) {
 		String name01 = "Jeong  in  Yun";
 		
-		//	Print the original value
-//		System.out.println(name01);
+//	Print the original value
+//	System.out.println(name01);
 		
-		// The first way of printing 
+// The first way of printing 
 		char [] result = delBlank(name01);
 		System.out.println(result);
 		String nameNew = new String(result);
 		System.out.println(nameNew);
 		
-		// The second way of printing the name without a blank
+// The second way of printing the name without a blank
 //		String name02 = "Jeong  in  Yun";
 //		System.out.println(name02.replace(" ", "") );
 	
 	}
 	
 	private static char[] delBlank( String name ) {
-//		char [] name = {'Jeong', ' ', 'in', ' ','Yun' };
-//		char [] newName = { 'Jeong', 'in', 'Yun' };
+//	char [] name = {'Jeong', ' ', 'in', ' ','Yun' };
+//	char [] newName = { 'Jeong', 'in', 'Yun' };
 		
 		int n = name.length();
 		char [] orgName = new char[ n ];
