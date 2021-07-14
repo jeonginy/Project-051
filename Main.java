fb8b6f37161f1689b3de2619785bb701b71b8639
jeonginy <86006588+jeonginy@users.noreply.github.com>
2021-07-14 18:15:50 +0900

    Add files via upload
    A test to find the maximum and minimum value in Array

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
-
 import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		//		it means : till when find the next value right after compiling the previous one
-		while(in.hasNextInt()) {
-			int A = in.nextInt();
-			int B = in.nextInt();
-			System.out.println( A + B);
+
+		int N = in.nextInt();
+		int[] arr = new int[N]; // create Array having
+		for (int i = 0; i < N; i++) { // to store the values of arr
+			arr[i] = in.nextInt();
 		}
+
+		int max = arr[0]; // declared maximum and minimum value at first
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++) { // to find the maximum value and minimum value
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+
+		}
+		System.out.print(min + " " + max);
+
 	}
 
 }
